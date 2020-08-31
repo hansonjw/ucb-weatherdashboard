7262fc068baf084c0aacc4d82ecdcd94b2076031
hansonjw <hansonjw@gmail.com>
2020-08-30 19:31:32 -0500

    Add data display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // DOM elements...
 var cityEl = document.querySelector("#city-to-search"); //
-
+var cityFormEl = document.querySelector("form");
 var currentEl = document.querySelector("#current-weather");
 var fiveDayEl = document.querySelector("#five-day");
 var prevCitiesEl = document.querySelector("#prev-cities");  //
@@ -9,7 +9,14 @@ var prevCitiesEl = document.querySelector("#prev-cities");  //
 var currentCityInfo = {};
 
 // An object to store previous queried city info
-var previousCityInfo = {};
+var previousCityInfo = [];
+
+
+// parse generate URL for icon
+function generateIcon(iconCode){
+    return "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+}
+
 
 // main data parsing function for the Weather day
 function parseWeatherData(weatherData, dataSource){
@@ -17,39 +24,37 @@ function parseWeatherData(weatherData, dataSource){
     
     if(dataSource == "current"){
         
-        currentCityInfo.cityWeather = weatherData["weather"]["0"]["description"];
         currentCityInfo.cityName = weatherData["name"];
-        currentCityInfo.cityTemp = weatherData["main"]["temp"];
-        currentCityInfo.cityDate = moment.unix(weatherData["dt"], "MM-DD-YYYY").format("dddd, MMMM Do YYYY");
-        currentCityInfo.cityIcon = weatherData["weather"]["0"]["icon"];
-        currentCityInfo.cityHumid = weatherData["main"]["humidity"];
-        currentCityInfo.cityWind = weatherData["wind"]["speed"];
+        currentCityInfo.cityDate = moment.unix(weatherData["dt"], "MM-DD-YYYY").format("dddd, MM/DD/YYYY");
+
         currentCityInfo.cityLat = weatherData["coord"]["lat"];
         currentCityInfo.cityLon = weatherData["coord"]["lon"];
 
-        console.log(currentCityInfo);
+        currentCityInfo.cityWeather = weatherData["weather"]["0"]["description"];
+        currentCityInfo.cityTemp = weatherData["main"]["temp"];    
+        currentCityInfo.cityHumid = weatherData["main"]["humidity"];
+        currentCityInfo.cityWind = weatherData["wind"]["speed"];
+
+        currentCityInfo.cityIcon = weatherData["weather"]["0"]["icon"];
 
     }
     else if(dataSource == "fiveDay"){
-        // um code...
-        console.log(weatherData);
 
         // create an array of the 5 day forecast data
         var foreCast = [];
-        for (i = 1; i <= 5; i++){
+        for (i = 0; i < 40; i=i+8){
             
             var day = {};
-
-            day.cityDate = moment.unix(weatherData["list"][i.toString()]["dt"]).format("dddd, MMMM Do YYYY");
+            day.cityDate = moment.unix(weatherData["list"][i.toString()]["dt"]).format("dddd, MM/DD/YYYY");
             day.cityIcon = weatherData["list"][i.toString()]["weather"]["0"]["icon"];
             day.cityTemp = weatherData["list"][i.toString()]["main"]["temp"];
             day.cityHumid = weatherData["list"][i.toString()]["main"]["humidity"];
 
             foreCast.push(day);
-            
         }
         // append the 5 day forecast array to the object
         currentCityInfo.foreCast = foreCast;
+        displayFiveDayForecast(currentCityInfo.foreCast);
 
     } 
     else if(dataSource == "uv"){
@@ -62,19 +67,17 @@ function parseWeatherData(weatherData, dataSource){
 
 
 function getWeatherData(city){
-    console.log("Current weather...");
 
     var apiKey = "f2b4d2c88506a785d23e14d41985d05f";
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
         + city + "&units=imperial&appid=" + apiKey;
 
-    console.log(apiUrl);
-    
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
                 parseWeatherData(data, "current");
+                // ************** Not sure this is a good code design...
                 // Go get UV data...this call has to be here becuase of the A-Synchronous behaviour    
                 getUvData(currentCityInfo.cityLat, currentCityInfo.cityLon);
                 
@@ -89,28 +92,22 @@ function getWeatherData(city){
 
 
 function getUvData(lat, lon){
-    // um code...
-    console.log("UV data, a lot of work for this...");
-    console.log(typeof lat, lat);
-    console.log(typeof lon, lon);
 
     var apiKey = "f2b4d2c88506a785d23e14d41985d05f";
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?"
         + "lat=" + lat.toString()
         + "&lon=" + lon.toString()
         + "&exclude=minutely,hourly&units=imperial&appid=" + apiKey;
-
-    console.log(apiUrl);
     
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
                 parseWeatherData(data, "uv");
-                console.log(currentCityInfo);
                 
                 //
                 // This is where you want to put the function call to "displayWeather()"
+                displayCurrentWeather(currentCityInfo);
                 //
 
             });
@@ -123,13 +120,10 @@ function getUvData(lat, lon){
 
 
 function getFiveDayData(city){
-    console.log("5 day forecast...");
 
     var apiKey = "f2b4d2c88506a785d23e14d41985d05f";
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="
         + city + "&units=imperial&appid=" + apiKey;
-
-    console.log(apiUrl);
     
     fetch(apiUrl).then(function(response) {
         // request was successful
@@ -146,29 +140,182 @@ function getFiveDayData(city){
 }
 
 
-function displayCurrentWeather(){
-    // umm code...
-    var weatherToPost = document.createElement('div');
-    //weatherToPost.textContent = ;
+function displayFiveDayForecast(forecastArr){
+    //receive an array of "days" that represent objects containing weather info..
 
-    currentEl.appendChild(weatherToPost);
+    var fiveDayTitle = document.createElement("h2");
+    fiveDayTitle.textContent = "Five Day Forecast";
+    document.querySelector("#five-day-title").appendChild(fiveDayTitle);
+
+    for (i=0; i < forecastArr.length; i++){
+
+        var dayForecast = document.createElement("div");
+        dayForecast.setAttribute("class", "card mb-4 shadow-sm");
+        fiveDayEl.appendChild(dayForecast);
+        
+        var dayDate = document.createElement("div");
+        dayDate.setAttribute("class", "card-header");
+        dayDate.textContent = forecastArr[i].cityDate;
+        dayForecast.appendChild(dayDate);
+
+        var dayIcon = document.createElement("img");
+        dayIcon.setAttribute("src", generateIcon(forecastArr[i].cityIcon));
+        dayForecast.appendChild(dayIcon);
+
+        var dayWeather = document.createElement("div");
+        dayWeather.setAttribute("class", "card-body");
+        dayForecast.appendChild(dayWeather);
+
+        var dayList = document.createElement("ul");
+        dayWeather.appendChild(dayList);
+
+        var listTemp = document.createElement("p");
+        listTemp.textContent = "Temperature: " + forecastArr[i].cityTemp;
+        dayList.appendChild(listTemp);
+
+        var listHumid = document.createElement("p");
+        listHumid.textContent = "Humidity: " + forecastArr[i].cityHumid;
+        dayList.appendChild(listHumid);
+
+    }
+
 }
 
-function displayFiveDayForecast(){
-    // umm code...
+
+function displayCurrentWeather(aCity){
+    // This is total brute force and I would look for another way to do this if I had time...
+    // New solution would involve restructing the OBJECT that all of the data is stored in
+    // and utilizing loops and arrays
+
+
+    var cardName = document.createElement("h2");
+    cardName.textContent = aCity.cityName;
+    document.querySelector("#current-city").appendChild(cardName);
+
+    var cardCurrent = document.createElement("div");
+    cardCurrent.setAttribute("class", "card mb-4 shadow-sm");
+
+    var cardDate = document.createElement("div");
+    cardDate.setAttribute("class", "card-header");
+    cardDate.textContent = aCity.cityDate;
+
+    var cardIcon = document.createElement("img");
+    cardIcon.setAttribute("src", generateIcon(aCity.cityIcon));
+
+    var cardWeather = document.createElement("div");
+    var cardList = document.createElement("ul");
+    cardWeather.appendChild(cardList);
+
+    // Adding Weather list items...I know this is bad code...
+    var listWeather = document.createElement("p");
+    listWeather.textContent = "Current Weather: " + aCity.cityWeather;
+    cardList.appendChild(listWeather);
+
+    var listTemp = document.createElement("p");
+    listTemp.textContent = "Temperature: " + aCity.cityTemp;
+    cardList.appendChild(listTemp);
+
+    var listHumid = document.createElement("p");
+    listHumid.textContent = "Humidity: " + aCity.cityHumid;
+    cardList.appendChild(listHumid);
+    
+    var listWind = document.createElement("p");
+    listWind.textContent = "Wind Speed: " + aCity.cityWind;
+    cardList.appendChild(listWind);
+
+    cardWeather.appendChild(cardList);
+    cardCurrent.appendChild(cardName);
+    cardCurrent.appendChild(cardDate);
+    cardCurrent.appendChild(cardIcon);
+    cardCurrent.appendChild(cardWeather);
+    currentEl.appendChild(cardCurrent);
+    
 }
 
+
+function displayPastCities(){
+
+    // Clear current display on site
+    jQuery(prevCitiesEl).empty();
+    
+    // Display current list of past searches, as buttons
+    for(i=0; i < previousCityInfo.length; i++){
+        var cityButton = document.createElement("button");
+        cityButton.setAttribute("class", "prev-city");
+        cityButton.setAttribute("type", "submit");
+        cityButton.value = previousCityInfo[i];
+        cityButton.textContent = previousCityInfo[i];
+        cityButton.setAttribute("id", previousCityInfo[i]);
+        prevCitiesEl.appendChild(cityButton);
+    }
+
+}
+
+
+function updateCityInfo() {
+
+    // stores current city
+    // clears existing display
+    // executes function to update sidebar display
+    // update local storage by storing previousCityInfo
+
+    var fromStorage = JSON.parse(localStorage.getItem("previousCityInfo"));
+    console.log(fromStorage);
+    if (fromStorage){
+        previousCityInfo = fromStorage;
+    }
+
+    if (Object.keys(currentCityInfo).length > 0){
+        
+        if (previousCityInfo.includes(currentCityInfo.cityName)==false){
+            previousCityInfo.push(currentCityInfo.cityName);
+        }
+        
+        localStorage.setItem("previousCityInfo", JSON.stringify(previousCityInfo));
+        currentCityInfo = {};
+
+        jQuery(currentEl).empty();
+        jQuery(fiveDayEl).empty();
+        
+        return;
+    }
+    else{
+        return;
+    }   
+
+}
 
 var formSubmitHandler = function(event) {
+
     event.preventDefault();
+    // console.log(event.target.id);
+
+    // capture current city
     var city = cityEl.value.trim();
     console.log(city);
+    cityEl.value = "";
+    
+    // Got get weather data
     getWeatherData(city);
     getFiveDayData(city);
 
-    // need to consider error handling here...
+    // reset and clear the display, store some data
+    updateCityInfo();
+    displayPastCities();
+
 }
 
 // Listen for button push
 cityFormEl.addEventListener("submit", formSubmitHandler);
+
+// Listen for other city buttons...
+jQuery(prevCitiesEl).on("click", "button", function(){
+    
+    var city = jQuery(this).attr("id");
+
+    // Go get weather data
+    getWeatherData(city);
+    getFiveDayData(city);
+
+});
 
